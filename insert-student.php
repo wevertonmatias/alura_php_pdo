d4d6de6108a2d13c4770317fc490e6b074d390ce
Weverton Matias <weverton-matias@hotmail.com>
2023-04-01 15:13:40 -0300

    Iniciando com o tratamento de instruções SQL, prepared statement.

--- insert-student.php
+++ insert-student.php
@@ -7,9 +7,20 @@ require_once 'vendor/autoload.php';
 $dbPath = __DIR__ . '/banco.sqlite';
 $pdo = new PDO("sqlite:{$dbPath}");
 
-$student = new Student(null, 'Weverton Matias', new DateTimeImmutable('1995-06-14'));
+//$student = new Student(null, 'Weverton Matias', new DateTimeImmutable('1995-06-14'));
+//
+//$sqlInsert = "INSERT INTO students (name, birth_date) VALUES (
+//                '{$student->name()}', '{$student->birthDate()->format('Y-m-d')}');";
 
-$sqlInsert = "INSERT INTO students (name, birth_date) VALUES (
-                '{$student->name()}', '{$student->birthDate()->format('Y-m-d')}');";
+//var_dump($pdo->exec($sqlInsert));
 
-var_dump($pdo->exec($sqlInsert));
+
+$student = new Student(null, "Weverton', ''); DROP TABLE students -- Matias", new DateTimeImmutable('1995-06-14'));
+
+$sqlInsert = "INSERT INTO students (name, birth_date) VALUES (?, ?)";
+$statement = $pdo->prepare($sqlInsert);
+$statement->bindValue(1, $student->name());
+$statement->bindValue(2, $student->birthDate()->format('Y-m-d'));
+if($statement->execute()){
+   echo 'Aluno inserido com sucesso.';
+};
