e5ae9612a7d1f302c001c5b32be2ba8e14fb6e24
Weverton Matias <weverton-matias@hotmail.com>
2023-04-01 14:34:32 -0300

    Pegando dado do banco e instanciando um obejto.

--- lista-alunos.php
+++ lista-alunos.php
@@ -1,10 +1,20 @@
 <?php
 
+use Alura\Pdo\Domain\Model\Student;
+
 require_once 'vendor/autoload.php';
 
 $dbPath = __DIR__ . '/banco.sqlite';
 $pdo = new PDO("sqlite:{$dbPath}");
 
 $statment = $pdo->query("SELECT * FROM students;");
+$studentDataList = $statment->fetchAll(PDO::FETCH_ASSOC);
+$studantList = [];
 
-var_dump($statment->fetchAll());
+foreach ($studentDataList as $studantData){
+    $studantList[] = new Student($studantData['id'],
+                        $studantData['name'],
+                        new DateTimeImmutable($studantData['birth_date']));
+}
+
+var_dump($studantList);
