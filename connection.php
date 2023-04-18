4b60477b827f0050c30fdc8b69194edd8fe8c1dc
Weverton Matias <weverton-matias@hotmail.com>
2023-04-18 20:23:47 -0300

    Criando o model Phone e adicionando referência no Student.

--- connection.php
+++ connection.php
@@ -8,4 +8,17 @@ $pdo = ConnectionCreator::CreateConnection();
 
 echo 'conectei';
 
-$pdo->exec('CREATE TABLE students (id INTEGER PRIMARY KEY,  name TEXT, birth_date TEXT);');
+$createTableSql = <<<SQL
+    CREATE TABLE IF NOT EXISTS students (id INTEGER PRIMARY KEY,
+    name TEXT,
+    birth_date TEXT);
+
+    CREATE TABLE IF NOT EXISTS phones(id INTEGER PRIMARY KEY,
+    area_code TEXT,
+    number TEXT,
+    student_id INTEGER,
+    FOREIGN KEY(student_id) REFERENCES students(id)); 
+SQL;
+
+
+$pdo->exec($createTableSql);
